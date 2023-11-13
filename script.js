1b1523289ab7881e82603363728b64c3e8b2593c
Abanteeka_Acharya <114434016+Abanteeka@users.noreply.github.com>
2023-11-13 22:53:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -175,7 +175,26 @@ img {
 
 
 /* Show menu */
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /* Add shadow header */
 
